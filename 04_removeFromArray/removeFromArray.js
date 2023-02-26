093891eac9742b8f0f01537af1bc4f610f0c892a
EduSTC <edustclair@gmail.com>
2023-02-26 19:54:30 -0300

    Finished the forth exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
 const removeFromArray = function() {
-
+    const arrayArgument = arguments[0];
+    for (let i = 0; i < arguments.length; i++) {
+        for (let j = 0; j < arrayArgument.length; j++) {
+            if (arguments[i] === arrayArgument[j]) {
+                arrayArgument.splice(j, 1);
+            }
+        }
+    }
+    return arrayArgument;
 };
 
 // Do not edit below this line
