40dc15e81d718fe77e10ce466780ecc4bda7b450
EduSTC <edustclair@gmail.com>
2023-02-26 19:35:51 -0300

    Finished the third exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    stringLetters = string.split("");
+    let lastIndex = stringLetters.length - 1;
+    let reverseString = "";
+    for (let i = lastIndex; i >= 0; i--) {
+        reverseString += stringLetters[i];
+    }
+
+    return reverseString;
 
 };
 
