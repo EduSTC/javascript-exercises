022a46c5dc0bac3ee2153503421204e78d5118be
EduSTC <edustclair@gmail.com>
2023-02-27 13:21:23 -0300

    Finished the seventh exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  let celsius = ((num - 32) * 5 / 9);
+  if (celsius != 0) {
+    return parseFloat(celsius.toFixed(1));
+  }
+  return celsius;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  let farenheit = (num * 9 / 5 + 32);
+  if (farenheit != 0) {
+    return parseFloat(farenheit.toFixed(1));
+  }
+  return farenheit;
 };
 
 // Do not edit below this line
