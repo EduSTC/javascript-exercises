e9a9afad48e8f54222b9dc2317c1679fa9fa0d8b
EduSTC <edustclair@gmail.com>
2023-02-27 13:03:41 -0300

    Finished the fifth exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,13 +1,23 @@
-const removeFromArray = function() {
-    const arrayArgument = arguments[0];
-    for (let i = 0; i < arguments.length; i++) {
-        for (let j = 0; j < arrayArgument.length; j++) {
-            if (arguments[i] === arrayArgument[j]) {
-                arrayArgument.splice(j, 1);
-            }
+// const removeFromArray = function() {
+//    const arrayArgument = arguments[0];
+//    for (let i = 0; i < arguments.length; i++) {
+//        for (let j = 0; j < arrayArgument.length; j++) {
+//            if (arguments[i] === arrayArgument[j]) {
+//                arrayArgument.splice(j, 1);
+//            }
+//        }
+//    }
+//    return arrayArgument;
+// };
+
+const removeFromArray = function(array, ...args) {
+    const newArray = [];
+    array.array.forEach(element => {
+        if (!(element.include(args))) {
+            newArray.push(element);
         }
-    }
-    return arrayArgument;
+    });
+    return newArray;
 };
 
 // Do not edit below this line
