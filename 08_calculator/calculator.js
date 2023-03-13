8530548dd754b67cd34a69dd0e43ecba155afd12
EduSTC <edustclair@gmail.com>
2023-03-13 13:07:17 -0300

    Finished the 8th exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,47 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arrayNum) {
+	let sum = 0;
+  arrayNum.forEach(element => {
+    sum += element;
+  });
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arrayNum) {
+  let product = 1;
+  for (let i = 0; i < arrayNum.length; i++) {
+    product *= arrayNum[i];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  if (num2 === 0) {
+    return 1;
+  }
+  let power = num1;
+	for (let i = 1; i < num2; i++) {
+    power *= num1;
+  }
+  return power;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0) {
+    return 1;
+  }
+  let factorial = 1;
+  for (let i = 1; i <= num; i++) {
+    factorial *= i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
