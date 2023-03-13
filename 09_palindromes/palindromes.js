d70dd94f4c217c5ab5dc30403a9e895bce2b52b4
EduSTC <edustclair@gmail.com>
2023-03-13 15:50:42 -0300

    Finished 9th exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    let newText = text.toLowerCase().replace(/[^a-z]/g, "").trim();
+    return newText.split("").reverse().join("") === newText;
 };
 
 // Do not edit below this line
