37103a2affe1c9b29a91ea521769020954d1d010
EduSTC <edustclair@gmail.com>
2023-03-14 15:13:30 -0300

    Finished the 12th exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(personArray) {
+    return personArray.reduce((personA, personB) => {
+        if (personA.yearOfDeath === undefined) {
+            personA.yearOfDeath = new Date().getFullYear();
+        } else if (personB.yearOfDeath === undefined) {
+            personB.yearOfDeath = new Date().getFullYear();
+        }
+        ageA = personA.yearOfDeath - personA.yearOfBirth;
+        ageB = personB.yearOfDeath - personB.yearOfBirth;
+        if (ageA > ageB) {
+            return personA;
+        } else {
+            return personB;
+        }
+        
 
+    });
 };
 
 // Do not edit below this line
