058966ced70f2ea9c5883b4d592f2fe51ba445ff
EduSTC <edustclair@gmail.com>
2023-03-14 14:32:28 -0300

    Finished the 10th exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(position) {
+    if (position < 0) {
+        return "OOPS";
+    }
+    let a = 0;
+    let b = 1;
+    for (let i = 0; i < position; i++) {
+        temp = a;
+        a = a + b;
+        b = temp;
+    }
+    return a;
 };
 
 // Do not edit below this line
